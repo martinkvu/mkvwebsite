04985e9d926949a8af0477a6f7b7adc8cd935524
Martin Vu <91714249+martinkvu@users.noreply.github.com>
2025-03-22 21:15:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
